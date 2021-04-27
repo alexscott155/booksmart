80683e2f6e6f291d350409729702e4dd4a2ae731
Alex Scott <atscottgh@gmail.com>
2021-04-27 12:20:49 -0400

    Add buggy random interest code

--- project-folder/src/app/services/book.service.ts
+++ project-folder/src/app/services/book.service.ts
@@ -14,12 +14,14 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class BookService {
+  interestsArray:InterestType[] = [];
   private interest:any
   private interests: Observable<InterestType[]>;
   private interestCollection: AngularFirestoreCollection<InterestType>;
   uid = this.firebaseService.uid
   interestsLength:any;
   books: string;
+  randomInterest:string;
   constructor( private http: HttpClient,
     private angularFirestore: AngularFirestore,
     public firebaseService: FirebaseService) { 
@@ -40,42 +42,48 @@ export class BookService {
    
     this.interests
     .subscribe( res => {
+        this.interestsArray = res;
+        console.log("INTERESTS ARRAY: ", this.interestsArray);
         this.interestsLength = res.length;
         var interestIndex = Math.floor(Math.random() * this.interestsLength);
         this.interest = res[interestIndex];
+        this.randomInterest= this.pickInterest()
      })
 
+  }
+
+  ionViewWillEnter() {
+    
   }
 
   ngOnInit() {
     
   }
 
-  public prepRequest(interest:any): Observable<object> {
+
+  public prepRequest(): Observable<object> {
     // this.interest
-    console.log(interest)
+    // console.log(interest)
     // this.pickBook(this.interests);
     // const dataUrl = "https://www.googleapis.com/books/v1/volumes?q=subject:"+this.interest.interest
-    const dataUrl = "https://www.googleapis.com/books/v1/volumes?q="+interest
+    // var randomInterest = this.pickInterest()
+    console.log("Choosen interest: " + this.randomInterest)
+    const dataUrl = "https://www.googleapis.com/books/v1/volumes?q="+this.randomInterest
     
     return this.http.get(dataUrl);
   }
 
-  async pickBook(interests: any){
+  pickInterest(){
     console.log(this.interestsLength)
-    var interestIndex = Math.floor(Math.random() * 2);
+    var interestIndex = Math.floor(Math.random() * this.interestsArray.length-1);
     var bookIndex = Math.floor(Math.random() * 10);
+
+    return this.interestsArray[interestIndex].interest
+
     // var interestInfo:any = this.getOpportunityByIndex(interestIndex);
   //  var test = await this.getOpportunityByIndex(interestIndex)
   //  console.log(test)
     // console.log(interestInfo.interest, interestInfo.length)
-    this.interests
-    .subscribe( res => {
-        this.interest = res[interestIndex];
-        this.prepRequest(res[interestIndex]?.interest)
-     })
-     console.log(this.interest)
-     
      
 
 
