aa4451d6f1a24e734cac117499eacc5ab8485f5a
Alex Scott <atscottgh@gmail.com>
2021-04-28 17:29:53 -0400

    Embed book previews on book detail page

--- project-folder/src/app/book-detail/book-detail.page.ts
+++ project-folder/src/app/book-detail/book-detail.page.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-book-detail',
@@ -8,15 +10,36 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class BookDetailPage implements OnInit {
   book:any
-  constructor(private router: Router, private route:ActivatedRoute) { }
+  bookId:any
+  parsedBook:any
+  embedBook:any
+  displayBook:any
+  safeResourceUrl: SafeResourceUrl;
+  constructor(
+    private router: Router, 
+    private route:ActivatedRoute,
+    private http: HttpClient,
+    private domSanitizer: DomSanitizer
+    ) {
+     
+     }
 
   ngOnInit() {
     this.route.params.subscribe(
   		book=>{
   			this.book = book;
   			console.log(this.book)
+        this.safeResourceUrl = this.domSanitizer.bypassSecurityTrustResourceUrl("https://books.google.com/books?id=" + this.book.id + "&newbks=0&lpg=PP1&dq=tea&pg=PP1&output=embed")
   		}
   		)
+
+        const dataUrl = this.book.selfLink
+        this.http.get(dataUrl).toPromise().then(res=>{
+          console.log("res in book detail:",res)
+          this.parsedBook = res;
+          this.embedBook = "https://books.google.com/books?id=" + this.parsedBook?.id + "&newbks=0&lpg=PP1&dq=tea&pg=PP1&output=embed"
+        })
+
   }
 
 }
