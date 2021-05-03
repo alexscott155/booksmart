e1f36164180e0008ce3b04a74cc1ca11b540e04a
ODhori Prioleau <odhoriprioleau@gmail.com>
2021-05-03 10:58:06 -0400

    Add Streak Page with manual reset

--- project-folder/src/app/streak/streak.page.ts
+++ project-folder/src/app/streak/streak.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+import { Storage } from '@ionic/storage-angular'
 
 @Component({
   selector: 'app-streak',
@@ -7,9 +9,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StreakPage implements OnInit {
 
-  constructor() { }
+  private currentStreak: number  = 0;
+  private highStreak: number = 0;
 
-  ngOnInit() {
+
+  constructor(private firebaseService: FirebaseService, private storage: Storage) {
+
+
+   }
+
+  async ngOnInit() {
+    await this.storage.create()
+    this.storage.get('originalTime').then((time) =>
+    {
+      if(time)
+      {
+        let originalDate = new Date(time);
+        let currentDate = new Date();
+
+        let difference = currentDate.getTime() - originalDate.getTime();
+        let days = difference / ( 1000 * 60 * 60 * 24);
+
+        this.currentStreak = Math.floor(days);
+      }
+    });
+
+    this.storage.get('highStreak').then((streak) => 
+    {
+      if(streak)
+      {
+        this.highStreak = streak;
+      }
+    })
+    
+    
+  }
+
+  Missed()
+  {
+    console.log('test')
+    if(this.currentStreak > this.highStreak)
+    {
+      this.highStreak = this.currentStreak;
+      this.storage.set('highScore', this.currentStreak);
+    }
+ 
+
+    this.currentStreak = 0;
+
+    let currentDate = new Date();
+    //YYYY-MM-DD
+
+    this.storage.set('originalTime', currentDate.toISOString());
+
+    //set a new start time
+
   }
 
 }
